e9ba4613252bd2ba6ce4042c6defc37f5aa70950
robertox85 <dimarco.roberto85@gmail.com>
2020-07-10 15:49:55 +0200

    Push code

--- master_2017/sidebar.php
+++ master_2017/sidebar.php
@@ -1,10 +1,10 @@
-  <h2>Key Facts</h2>
+  <div class="clearfix sidebar-2">
+      <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Updates') ) : ?>
+     <?php endif; ?>
+  </div>
   <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Custom') ) : ?>
     <?php endif; ?>
-          <div class="clearfix sidebar-2">
-  <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Updates') ) : ?>
-  <?php endif; ?>
-</div>
+  
         
 <?php
    $blog_id = get_current_blog_id();
