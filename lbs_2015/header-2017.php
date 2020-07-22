81055f4e13da86dd821c5f55f5af771e727d4cf4
Flavio Camilli <flavio.camilli@pezzilli.com>
2020-07-22 14:39:16 +0200

    Update header-2017.php

--- lbs_2015/header-2017.php
+++ lbs_2015/header-2017.php
@@ -13,6 +13,7 @@
 
 ?>
 </title>
+    <!-- TEST -->
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-63172324-1"></script>
 <script>
