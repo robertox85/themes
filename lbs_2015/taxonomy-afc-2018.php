85e41c5383f26f2e8d033cf8b06ac4062e7a80d4
Flavio Camilli <flavio.camilli@pezzilli.com>
2020-07-22 17:24:31 +0200

    Update taxonomy-afc-2018.php

--- lbs_2015/taxonomy-afc-2018.php
+++ lbs_2015/taxonomy-afc-2018.php
@@ -78,7 +78,7 @@ Full time - 24
     <div class="tab-content" id="ProgramType">
         <!-- MASTER -->
         <div class="tab-pane fade active in" id="exmasters" role="tabpanel" aria-labelledby="exmasters-tab">
-            <div class="row"></div>
+            <div class="row">
             <div class="col-12 col-lg-8">
             <h2><?php echo get_field('etichetta_executive_master') ?></h2>
             <p><?php echo get_field('campo_executive_master') ?></p>
