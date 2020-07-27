68394537d30a05f53c4cef3e411726c5123101c5
Flavio Camilli <flavio.camilli@pezzilli.com>
2020-07-27 13:09:39 +0200

    Update card-course.php

--- lbs_2015/pco/template-parts/card-course.php
+++ lbs_2015/pco/template-parts/card-course.php
@@ -15,23 +15,7 @@
             <?php else : ?>
             <?php endif; ?></strong> <?php the_field('testo_inizio'); ?></p>
     <?php endif; ?>
-    <?php if(get_field('data_inizio')): ?>
-                    <span><?php the_terms($post->ID, 'lunghezza_corsi', $before = '', $sep = ', ', $after = '' ); ?></span>
-                    <h6><?php the_title(); ?></h6>
-                    <?php if( get_field('durata') ): ?>
-                    <p><strong>
-                            <?php if (ICL_LANGUAGE_CODE=='it') : ?>Durata
-                            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Duration
-                            <?php else : ?>
-                            <?php endif; ?></strong> <?php the_field('durata'); ?></p>
-                    <?php endif; ?>
-                    <!-- Sede/Inizio Roma -->
-                    <?php if( get_field('testo_inizio') ): ?>
-                    <p><strong><?php if (ICL_LANGUAGE_CODE=='it') : ?>Sede
-                            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Location
-                            <?php else : ?>
-                            <?php endif; ?></strong> <?php the_field('testo_inizio'); ?></p>
-                    <?php 
+<?php 
 // Load field value.
 $date_string = get_field('data_inizio');
 // Create DateTime object from value (formats must match).
@@ -43,39 +27,7 @@ $date = DateTime::createFromFormat('Ymd', $date_string);
             <?php else : ?>
             <?php endif; ?> </strong> <?php echo $date->format('j M Y'); ?></p>
     <?php endif; ?>
-    <!-- Sede/Inizio Milano -->
-    <?php if( get_field('data_inizio_testuale') ): ?>
-    <p><strong><?php if (ICL_LANGUAGE_CODE=='it') : ?>Sede
-            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Location
-            <?php else : ?>
-            <?php endif; ?></strong> <?php the_field('data_inizio_testuale'); ?></p>
-    <?php endif; ?>
-    <?php if(get_field('data_fine')): ?>
-    <p><strong><?php if (ICL_LANGUAGE_CODE=='it') : ?>Inizio
-            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Start
-            <?php else : ?>
-            <?php endif; ?></strong> <?php echo he_field('data_fine'); ?></p>
-    <?php endif; ?>
-    <!-- Sede/Inizio Belluno -->
-    <?php if(get_field('veneto') ): ?>
-    <p><strong><?php if (ICL_LANGUAGE_CODE=='it') : ?>Sede
-            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Location
-            <?php else : ?>
-            <?php endif; ?></strong> <?php the_field('veneto'); ?></p>
-    <?php endif; ?>
     
-    <?php if(get_field('data_veneto')): ?>
-    <?php endif; ?>
-    <p><strong><?php if (ICL_LANGUAGE_CODE=='it') : ?>Inizio
-            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Start
-            <?php else : ?>
-            <?php endif; ?> </strong> <?php echo the_field('data_veneto'); ?></p>
-                    <p><strong><?php if (ICL_LANGUAGE_CODE=='it') : ?>Inizio
-                            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Start
-                            <?php else : ?>
-                            <?php endif; ?> </strong> <?php echo $date->format('j M Y'); ?></p>
-                    <?php endif; ?>
-
                     <!-- Sede/Inizio Milano -->
                     <?php if( get_field('data_inizio_testuale') ): ?>
                     <p><strong><?php if (ICL_LANGUAGE_CODE=='it') : ?>Sede
