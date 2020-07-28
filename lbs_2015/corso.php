d1a821bf52dcbd830401804c94e65f21d29fec97
Flavio Camilli <flavio.camilli@pezzilli.com>
2020-07-28 23:14:03 +0200

    Update corso.php

--- lbs_2015/corso.php
+++ lbs_2015/corso.php
@@ -12,11 +12,16 @@ get_header(2017);
 
 <style>
     @media (min-width: 1200px) {
-.container, .container-lg, .container-md, .container-sm, .container-xl {
-    max-width: 1440px;
-}
+
+        .container,
+        .container-lg,
+        .container-md,
+        .container-sm,
+        .container-xl {
+            max-width: 1440px;
+        }
     }
-    
+
     h1.entry-title {
         font-size: 4rem;
         line-height: 4.2rem;
@@ -24,33 +29,33 @@ get_header(2017);
 </style>
 
 <header class="bg__white">
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-lg-8 mt-48">
-        <h1 class="entry-title"><?php echo the_title(); ?></h1>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-lg-8 mt-48">
+                <h1 class="entry-title"><?php echo the_title(); ?></h1>
+            </div>
+        </div>
     </div>
-  </div>
 </header>
 
 <div class="container-fluid">
-<?php get_template_part( 'pco/template-parts/section-keyfacts', get_post_format() ); ?>
+    <?php get_template_part( 'pco/template-parts/section-keyfacts', get_post_format() ); ?>
 </div>
 
 <div class="container">
-  <div class="row">
-  <main id="main" class="site-main site-master col-lg-9 col-md-8" role="main">
-			  <div id="page-content" class="columns  space">
-        <?php while ( have_posts() ) : the_post(); ?>
-        <?php the_content(''); ?>
-         <p> <?php edit_post_link('<strong>Modifica Pagina</strong>', ''); ?></p>
-        <?php endwhile; // end of the loop. ?>
-      </div>
-    </main>
-    <aside class="col-lg-3 col-md-4 col-sm-12 space sidebar-master">
-		<div class="block">
-                        <div class="textwidget">
-                            <?php
+    <div class="row">
+        <main id="main" class="site-main site-master col-lg-9 col-md-8" role="main">
+            <div id="page-content" class="columns  space">
+                <?php while ( have_posts() ) : the_post(); ?>
+                <?php the_content(''); ?>
+                <p> <?php edit_post_link('<strong>Modifica Pagina</strong>', ''); ?></p>
+                <?php endwhile; // end of the loop. ?>
+            </div>
+        </main>
+        <aside class="col-lg-3 col-md-4 col-sm-12 space sidebar-master">
+            <div class="block">
+                <div class="textwidget">
+                    <?php
                        if ( get_field( 'brochure' ) ) {
 						echo '<span>' . get_field( 'brochure' ) . '</span>';
 						   
@@ -66,33 +71,32 @@ get_header(2017);
 					}
 
 					?>
+                </div>
 
-				<?php
+                <?php
 				if ( get_field( 'titolo_custom' ) ) {
 					echo '<div class="block-dark"><h3>' . get_field( 'titolo_custom' ) . '</h3><div class="textwidget"><p>' . get_field( 'campo_custom_contenuto' ) . '</p></div> </div>';
 				}
 				?>
 
-				<?php
+                <?php
 				if ( get_field( 'titolo_campo_custom_2' ) ) {
 					echo '<div class="block-dark"><h3>' . get_field( 'titolo_campo_custom_2' ) . '</h3><div class="textwidget"><p>' . get_field( 'campo_custom_2' ) . '</p></div> </div>';
 				}
 				?>
-   </div>
-                    </div>
 
-			<?php   
+            </div>
+
+            <?php   
      if (get_field('contatti') || get_field('contatti_campo'));
 	 echo '<div class="block-dark">';
 	 echo '<h3>'.get_field('contatti').'</h3><div class="textwidget">';
 	 echo get_field('contatti_campo');
      echo '</div></div>';
  ?>
-		</aside>
-  </div>
-  <!--END .row --> 
+        </aside>
+    </div>
 </div>
-<!--END .container -->
 <!--<script src="<?php // echo get_bloginfo('template_url') ?>/js/bootstrap.min.js"></script>-->
 <?php 
 
