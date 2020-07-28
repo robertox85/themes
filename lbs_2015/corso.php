5ff4430158287a822aa91b68f1ee9e9205ef08a3
Flavio Camilli <flavio.camilli@pezzilli.com>
2020-07-28 23:30:56 +0200

    Update corso.php

--- lbs_2015/corso.php
+++ lbs_2015/corso.php
@@ -82,7 +82,7 @@ get_header(2017);
 <div class="container">
     <p><?php edit_post_link('<strong>Modifica Pagina</strong>', ''); ?></p>
     <div class="row">
-        <main id="main" role="main" class="col-xs-12 col-md-8" >
+        <main id="main" role="main" class="col-xs-12 col-md-8 pr-md-48 pr-0" >
             <div id="page-content">
                 <?php while ( have_posts() ) : the_post(); ?>
                 <?php the_content(''); ?>
