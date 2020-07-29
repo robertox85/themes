66bf06da2badaf91485f5690a308859424512625
Flavio Camilli <flavio.camilli@pezzilli.com>
2020-07-29 11:42:29 +0200

    Update section-keyfacts.php

--- lbs_2015/pco/template-parts/section-keyfacts.php
+++ lbs_2015/pco/template-parts/section-keyfacts.php
@@ -5,10 +5,14 @@
             <div class="d-flex flex-row flex-lg-nowrap flex-wrap justify-content-start align-items-start align-items-lg-center">
                 <?php  
                     $date = DateTime::createFromFormat('Ymd', get_field('data_inizio'));	
+                
+                if(get_field('data_inizio')) {
 		
                     if(get_field('testo_inizio')) { echo '<div class="block pr-16  pr-lg-48"><p class="color__white text-uppercase p-0 mt-lg-32 mb-lg-16 mt-16 mb-8">' . get_field('testo_inizio') . '</p>';}
 			
                     if (get_field('data_inizio')) {  echo '<div class="textwidget"><h6 class="color__white mt-0 mb-32">' . $date->format('d M Y') . '</h6></div> </div>';}
+                }
+                
 		
                     $date_m = DateTime::createFromFormat('Ymd', get_field('data_fine'));	
 		
@@ -17,10 +21,14 @@
                     if (get_field('data_fine')) {  echo '<div class="textwidget"><h6 class="color__white mt-0 mb-32"' . $date_m->format('d M Y') . '</h6></div> </div>';}
 		
                     $date_v = DateTime::createFromFormat('Ymd', get_field('data_veneto'));	
+                
+                if(get_field('data_veneto')) {
 
                     if(get_field('veneto')) { echo '<div class="block pr-16  pr-lg-48"><p class="color__white text-uppercase p-0 mt-lg-32 mb-lg-16 mt-16 mb-8">' . get_field('veneto') . '</p>';}
 			
                     if(get_field('data_veneto')) {  echo '<div class="textwidget"><h6 class="color__white mt-0 mb-32"' . $date_v->format('d M Y') . '</h6></div> </div>';}
+                    
+                    }
 	
                     if(get_field('titolo_costo')) {
                     echo '<div class="block pr-16  pr-lg-48"><p class="color__white text-uppercase p-0 mt-lg-32 mb-lg-16 mt-16 mb-8">'. get_field('titolo_costo').'</p><div class="textwidget"><h6 class="color__white mt-0 mb-32">'. get_field('costo').'</h6></div></div>';
