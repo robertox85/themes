e06db5a75bfc60aa8bad920ed77cae67562797bf
Flavio Camilli <flavio.camilli@pezzilli.com>
2020-07-29 09:54:59 +0200

    Update section-keyfacts.php

--- lbs_2015/pco/template-parts/section-keyfacts.php
+++ lbs_2015/pco/template-parts/section-keyfacts.php
@@ -10,19 +10,19 @@
   
 	  $date = DateTime::createFromFormat('Ymd', get_field('data_inizio'));	
 		
-	 if(get_field('testo_inizio')) { echo '<div class="block"><h3 class="color__white">' . get_field('testo_inizio') . '</h3>';}
+	 if(get_field('testo_inizio')) { echo '<div class="block"><h6 class="color__white">' . get_field('testo_inizio') . '</h6>';}
 			
 	if (get_field('data_inizio')) {  echo '<div class="textwidget"><p class="color__white">' . $date->format('d/m/Y') . '</p></div> </div>';}
 		
 		  $date_m = DateTime::createFromFormat('Ymd', get_field('data_fine'));	
 		
-		 if(get_field('data_inizio_testuale')) { echo '<div class="block"><h3 class="color__white">' . get_field('data_inizio_testuale') . '</h3>';}
+		 if(get_field('data_inizio_testuale')) { echo '<div class="block"><h6 class="color__white">' . get_field('data_inizio_testuale') . '</h6>';}
 			
 	if (get_field('data_fine')) {  echo '<div class="textwidget"><p class="color__white">' . $date_m->format('d/m/Y') . '</p></div> </div>';}
 		
 		  $date_v = DateTime::createFromFormat('Ymd', get_field('data_veneto'));	
 		
-		 if(get_field('veneto')) { echo '<div class="block"><h3 class="color__white">' . get_field('veneto') . '</h3>';}
+		 if(get_field('veneto')) { echo '<div class="block"><h6 class="color__white">' . get_field('veneto') . '</h6>';}
 			
 	if (get_field('data_veneto')) {  echo '<div class="textwidget"><p class="color__white">' . $date_v->format('d/m/Y') . '</p></div> </div>';}
 		
@@ -31,23 +31,23 @@
 	
 				if(get_field('titolo_costo'))
 	{
-echo '<div class="block"><h3 class="color__white">'. get_field('titolo_costo').'</h3><div class="textwidget"><p>'. get_field('costo').'</p></div> </div>';
+echo '<div class="block"><h6 class="color__white">'. get_field('titolo_costo').'</h6><div class="textwidget"><p>'. get_field('costo').'</p></div> </div>';
     }
 			
-			 if(get_field('titolo_durata')) { echo '<div class="block"><h3 class="color__white">'. get_field('titolo_durata').'</h3>' ; }
+			 if(get_field('titolo_durata')) { echo '<div class="block"><h6 class="color__white">'. get_field('titolo_durata').'</h6>' ; }
              if(get_field('durata')) { echo '<div class="textwidget"><p class="color__white">'. get_field('durata') .'</p></div> </div>'; }
 			
 			
 						if(get_field('codice_corso'))
 	{
-echo '<div class="block"><h3 class="color__white">'. get_field('testo_codice_corso').'</h3><div class="textwidget"><p class="color__white">'. get_field('codice_corso').'</p></div> </div>';
+echo '<div class="block"><h6 class="color__white">'. get_field('testo_codice_corso').'</h6><div class="textwidget"><p class="color__white">'. get_field('codice_corso').'</p></div> </div>';
     }
 		 ?>
 
 
                 <?php
 				if ( get_field( 'key_facts' ) ) {
-					echo '<div class="block"><h3 class="color__white">' . get_field( 'key_facts' ) . '</h3><div class="textwidget"><p class="color__white">' . get_field( 'campo_key_facts' ) . '</p></div> </div>';
+					echo '<div class="block"><h6 class="color__white">' . get_field( 'key_facts' ) . '</h6><div class="textwidget"><p class="color__white">' . get_field( 'campo_key_facts' ) . '</p></div> </div>';
 				}
 
 				?>
