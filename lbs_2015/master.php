05718db7f5a6103a3ebe734701b79d970e825619
Flavio Camilli <flavio.camilli@pezzilli.com>
2020-07-29 13:31:41 +0200

    Update master.php

--- lbs_2015/master.php
+++ lbs_2015/master.php
@@ -63,25 +63,6 @@ Creation Date: 12/20/2018
 	<div class="container">
 	    <!-- TAB -->
 	    <section>
-            
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-  <li class="nav-item">
-    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
-  </li>
-</ul>
-<div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
-  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
-</div>
-            
-            
 	        <!-- NAVIGAZIONE -->
 	        <ul class="nav nav-tabs auto" id="program-type" role="tablist">
 	            <!-- ROMA -->
@@ -97,7 +78,7 @@ Creation Date: 12/20/2018
 	        <!-- CONTENUTO -->
 	        <div class="tab-content" id="ProgramType">
 	            <!-- ROMA -->
-	            <div class="tab-pane fade active show" id="roma" role="tabpanel" aria-labelledby="roma-tab">
+	            <div class="tab-pane fade active in" id="roma" role="tabpanel" aria-labelledby="roma-tab">
 	                <div class="row">
 	                    <div class="col-xs-12 col-lg-8">
 	                        <h2><?php echo $sede_rm[ICL_LANGUAGE_CODE] ?></h2>
