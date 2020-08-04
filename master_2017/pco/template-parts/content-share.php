27385bdd166802d4432beb0b9cbd101084957f7e
robertox85 <dimarco.roberto85@gmail.com>
2020-08-04 11:53:49 +0200

    Push code

--- master_2017/pco/template-parts/content-share.php
+++ master_2017/pco/template-parts/content-share.php
@@ -1,7 +1,7 @@
 <div class="share row d-none d-lg-block">
-    <div class="col-md-12 px-0 d-flex">
+    <div class="col-md-12 px-14 d-flex border-bottom-share">
         <small class="meta">SHARE</small>
-        <ul class="list-unstyled list-inline">
+        <ul class="list-unstyled list-inline ">
             <li class="list-inline-item"><a href="#" onclick="event.preventDefault();socialWindow( '<?php echo urlencode(get_permalink()) ?>', '<?php get_the_title() ?>' , 'facebook' )" id="facebook"><i class="fab fa-facebook-f"></i></a></li>
             <li class="list-inline-item"><a href="#" onclick="event.preventDefault();socialWindow( '<?php echo urlencode(get_permalink()) ?>', '<?php get_the_title() ?>' , 'twitter' )"  id="twitter"><i class="fab fa-twitter"></i></a></li>
             <li class="list-inline-item"><a href="#" onclick="event.preventDefault();socialWindow( '<?php echo urlencode(get_permalink()) ?>', '<?php get_the_title() ?>' , 'linkedin' )" id="linkedin"><i class="fab fa-linkedin-in"></i></a></li>
