7849654f0cf28d88837f76b64c1699986d596d2d
robertox85 <dimarco.roberto85@gmail.com>
2020-09-02 13:14:50 +0200

    Push code

--- lbs_2015/pco/template-parts/card-course.php
+++ lbs_2015/pco/template-parts/card-course.php
@@ -1,4 +1,5 @@
-<?php if(is_page_template(array(
+<?php if(is_page_template(
+    array(
         'executive-program-2018.php',
         'taxonomy-afc-2018.php',
         'taxonomy-banche-2018.php',
@@ -13,7 +14,9 @@
         'taxonomy-soft-2018.php',
         'taxonomy-sport-2018.php',
         'taxonomy-tax-2018.php',
-))) : ?>
+    )
+)
+) : ?>
 <style>
     :root {
         --current-color: #0077c8;
@@ -41,15 +44,15 @@
 <?php endif; ?>
 
 <div class="card-course-small p-24 my-16 d-flex flex-column justify-content-start align-items-start" style="border-color:var(--current-color);">
-    <span class="meta text-uppercase color__grey"><?php the_terms($post->ID, 'lunghezza_corsi', $before = '', $sep = ', ', $after = '' ); ?></span>
+    <span class="meta text-uppercase color__grey"><?php the_terms($post->ID, 'lunghezza_corsi', $before = '', $sep = ', ', $after = ''); ?></span>
 
     <h6 class="py-24 mb-0 each-word"><?php the_title(); ?></h6>
 
     <!-- Durata -->
-    <?php if( get_field('durata') ): ?>
+    <?php if(get_field('durata') ) : ?>
     <p class="big mb-8">
             <?php if (ICL_LANGUAGE_CODE=='it') : ?>Durata
-            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Duration
+            <?php elseif (ICL_LANGUAGE_CODE=='en' ) :?>Duration
             <?php else : ?>
             <?php endif; ?>
         <strong><?php the_field('durata'); ?></strong>
@@ -57,91 +60,91 @@
     <?php endif; ?>
 
     <!-- Sede Roma -->
-    <?php if( get_field('testo_inizio') ): ?>
+    <?php if(get_field('testo_inizio') ) : ?>
 
     <p class="big mb-8">
             <?php if (ICL_LANGUAGE_CODE=='it') : ?>Sede
-            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Location
+            <?php elseif (ICL_LANGUAGE_CODE=='en' ) :?>Location
             <?php else : ?>
             <?php endif; ?>
         <strong><?php the_field('testo_inizio'); ?></strong>
 
     <!-- Inizio Roma -->
 
-    <?php 
-// Load field value.
-$date_string_rome = get_field('data_inizio');
-// Create DateTime object from value (formats must match).
-$date_rome = DateTime::createFromFormat('Ymd', $date_string_rome);
-// Output current date in custom format.
-?><span>   -   </span>
+        <?php 
+        // Load field value.
+        $date_string_rome = get_field('data_inizio');
+        // Create DateTime object from value (formats must match).
+        $date_rome = DateTime::createFromFormat('Ymd', $date_string_rome);
+        // Output current date in custom format.
+        ?><span>   -   </span>
             <?php if (ICL_LANGUAGE_CODE=='it') : ?>Inizio
-            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Start
+            <?php elseif (ICL_LANGUAGE_CODE=='en' ) :?>Start
             <?php else : ?>
             <?php endif; ?>
         <strong><?php echo $date_rome->format('j M Y'); ?></strong></p>
 
-<?php endif; ?>
+    <?php endif; ?>
     <!-- Sede Milano -->
-    <?php if( get_field('data_inizio_testuale') ): ?>
+    <?php if(get_field('data_inizio_testuale') ) : ?>
 
     <p class="big mb-8">
             <?php if (ICL_LANGUAGE_CODE=='it') : ?>Sede
-            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Location
+            <?php elseif (ICL_LANGUAGE_CODE=='en' ) :?>Location
             <?php else : ?>
             <?php endif; ?>
         <strong><?php the_field('data_inizio_testuale'); ?></strong>
 
     <!-- Inizio Milano -->
 
-    <?php 
-// Load field value.
-$date_string_mi = get_field('data_fine');
-// Create DateTime object from value (formats must match).
-$date_mi = DateTime::createFromFormat('Ymd', $date_string_mi);
-// Output current date in custom format.
-?><span>   -   </span>
+        <?php 
+        // Load field value.
+        $date_string_mi = get_field('data_fine');
+        // Create DateTime object from value (formats must match).
+        $date_mi = DateTime::createFromFormat('Ymd', $date_string_mi);
+        // Output current date in custom format.
+        ?><span>   -   </span>
             <?php if (ICL_LANGUAGE_CODE=='it') : ?>Inizio
-            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Start
+            <?php elseif (ICL_LANGUAGE_CODE=='en' ) :?>Start
             <?php else : ?>
             <?php endif; ?>
         <strong><?php echo $date_mi->format('j M Y'); ?></strong></p>
     <?php endif; ?>
 
     <!-- Sede Belluno -->
-    <?php if( get_field('veneto') ): ?>
+    <?php if(get_field('veneto') ) : ?>
 
     <p class="big mb-8">
             <?php if (ICL_LANGUAGE_CODE=='it') : ?>Sede
-            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Location
+            <?php elseif (ICL_LANGUAGE_CODE=='en' ) :?>Location
             <?php else : ?>
             <?php endif; ?>
         <strong><?php the_field('veneto'); ?></strong>
 
     <!-- Inizio Belluno -->
 
-    <?php 
-// Load field value.
-$date_string_be = get_field('data_veneto');
-// Create DateTime object from value (formats must match).
-$date_be = DateTime::createFromFormat('Ymd', $date_string_be);
-// Output current date in custom format.
-?><span>   -   </span>
+        <?php 
+        // Load field value.
+        $date_string_be = get_field('data_veneto');
+        // Create DateTime object from value (formats must match).
+        $date_be = DateTime::createFromFormat('Ymd', $date_string_be);
+        // Output current date in custom format.
+        ?><span>   -   </span>
             <?php if (ICL_LANGUAGE_CODE=='it') : ?>Inizio
-            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Start
+            <?php elseif (ICL_LANGUAGE_CODE=='en' ) :?>Start
             <?php else : ?>
             <?php endif; ?>
         <strong><?php echo $date_be->format('j M Y'); ?></strong></p>
-<?php endif; ?>
+    <?php endif; ?>
     
     <!-- Lingua -->
     <p class="big mb-8"><?php if (ICL_LANGUAGE_CODE=='it') : ?>Lingua
-            <?php elseif ( ICL_LANGUAGE_CODE=='en' ) :?>Language
+            <?php elseif (ICL_LANGUAGE_CODE=='en' ) :?>Language
             <?php else : ?>
-            <?php endif; ?><strong><?php $terms = get_the_terms( $post->ID, 'lingue' );
-                foreach ( $terms as $term ) {
-  echo '<span class="text-capitalize">' . strip_tags(term_description( $term->term_id, 'lingue')) . '</span>';
-                } ?></strong></p>
+            <?php endif; ?><strong><?php $terms = get_the_terms($post->ID, 'lingue');
+            foreach ( $terms as $term ) {
+                echo '<span class="text-capitalize">' . strip_tags(term_description($term->term_id, 'lingue')) . '</span>';
+            } ?></strong></p>
 
     <!-- Pulsanti -->
     <div class="mt-24 d-flex flex-row justify-content-between align-items-stretch flex-wrap flex-lg-nowrap  ctas">
